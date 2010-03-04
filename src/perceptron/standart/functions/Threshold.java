3ea9c23af5050a309b7d8009d2e88a3e12ea53fb
Ilya G Mirin <gpdribbler@gmail.com>
2010-03-04 18:16:28 +1000

    Добавил пороговую передаточную функцию.

---
+++ src/perceptron/standart/functions/Threshold.java
@@ -0,0 +1,31 @@
+package perceptron.standart.functions;
+
+public class Threshold implements ITransferFunction {
+
+    private double threshold;
+
+    public Threshold() {
+        this.threshold = 0.5;
+    }
+
+    public Threshold(double threshold) {
+        this.threshold = threshold;
+    }
+
+    public double transfer(double sum) {
+        if(sum > threshold) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }//else if
+    }//transfer
+
+    public double getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(double threshold) {
+        this.threshold = threshold;
+    }
+
+}
