2195e3442dd9d48aea9f3f48f2db6e79c6ba12de
Ilya G Mirin <gpdribbler@gmail.com>
2010-03-29 22:19:52 +1100

    'Add_convolutioner_class.'

---
+++ src/convolutioner/Convolutioner.java
@@ -0,0 +1,19 @@
+package convolutioner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import perceptron.Perceptron;
+
+//TODO: написать сверточника
+//TODO: написать субдескретизатора как нейронку
+public class Convolutioner {
+
+    private Set<Perceptron> kernels;
+    
+    public List<Double[]> convolute() {
+        List<Double[]> result = new ArrayList<Double[]>();
+        return result;
+    }
+
+}
